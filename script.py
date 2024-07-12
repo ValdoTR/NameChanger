33675a1dddee98d057a05547fb4b75747535e01a
Valdo <vteixeiraromao@gmail.com>
2024-07-12 23:33:49 +0100

    Add command parameters feature

--- script.py
+++ script.py
@@ -1,11 +1,10 @@
 import os
 import shutil
+import sys
 
 # Constants
 INPUT_DIR = 'input'  # Input directory containing files
 OUTPUT_DIR = 'output'  # Output directory to save renamed files
-STRING_TO_REPLACE = 'foo'  # String to be replaced
-REPLACEMENT_STRING = 'bar'  # String to replace with
 
 def rename_files_in_directory(input_dir, output_dir, string_to_replace, replacement_string):
     """
@@ -16,7 +15,12 @@ def rename_files_in_directory(input_dir, output_dir, string_to_replace, replacem
         output_dir (str): Path to the output directory.
         string_to_replace (str): The string to be replaced in file names.
         replacement_string (str): The string to replace with in file names.
+    
+    Returns:
+        int: Number of files renamed.
     """
+    files_renamed = 0
+    
     for root, _, files in os.walk(input_dir):
         for file in files:
             # Construct full file path
@@ -36,15 +40,38 @@ def rename_files_in_directory(input_dir, output_dir, string_to_replace, replacem
             
             # Copy the file to the new location with the new name
             shutil.copy2(input_file_path, output_file_path)
-            
-            print(f"Renamed {input_file_path} to {output_file_path}")
+
+            # Check if the string to replace is in the file name
+            if string_to_replace in file:
+                print(f"Renamed '{input_file_path}' to '{output_file_path}'")
+                files_renamed += 1
+
+    return files_renamed
+
 
 def main():
     """
     Main function to execute the file renaming process.
     """
-    rename_files_in_directory(INPUT_DIR, OUTPUT_DIR, STRING_TO_REPLACE, REPLACEMENT_STRING)
-    print("\033[92mFile renaming completed.\033[0m")
+    if len(sys.argv) != 3:
+        print("Please run: python3 script.py [string_to_replace] [replacement_string]")
+        sys.exit(1)
+    
+    string_to_replace = sys.argv[1]
+    replacement_string = sys.argv[2]
+
+    files_renamed = rename_files_in_directory(INPUT_DIR, OUTPUT_DIR, string_to_replace, replacement_string)
+    
+    if files_renamed > 0:
+        print(f"\033[92m{files_renamed} files have been renamed.\033[0m")
+    else:
+        print(f"\033[93mNo files have been renamed.\033[0m")
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\033[91mScript cancelled by user.\033[0m")
+    except Exception as e:
+        print(f"\033[91mAn error occurred: {e}\033[0m")
